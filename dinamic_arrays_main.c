d61f6df52a7019e174d5011f7f17b8102d0352ab
MauroPresso <mauro.presso@gmail.com>
2024-07-25 21:38:25 -0300

    This is the first version of dinamic_arrays_main.c

---
+++ dinamic_arrays_main.c
@@ -0,0 +1,80 @@
+#include "dinamic_arrays_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+
+void llenar_int_vector(int *vec, uint8_t cant);
+
+int main()
+{
+    
+    // int_vector
+
+    int* v = (int*)malloc(10*sizeof(int));
+    int* u = (int*)malloc(5*sizeof(int));
+
+    // vector dinamico V
+    llenar(v, 10);
+    printf("\nVector V:\n");
+    mostrar(v, 10);
+
+    // vector dinamico U
+    llenar(u, 5);
+    printf("\nVector U:\n");
+    mostrar(u, 5);
+
+    // Concatenar
+    int *cad = new int[15];
+    cad = concat(v, 10, u, 5);
+    printf("\nVector concatenado:\n");
+    mostrar(cad, 15);
+    // It works OK.
+
+    // Resizing vector V.
+    // new_size < old_size
+    v = resize(v, 10, 5);
+    printf("\nVector V resized (new_size < old_size):\n");
+    mostrar(v, 5);
+    // It works OK.
+    
+    // Resizing vector V.
+    // new_size > old_size
+    v = resize(v, 5, 10);
+    printf("\nVector V resized (new_size > old_size):\n");
+    llenar(v, 10);
+    mostrar(v, 10);
+    // It works OK.
+
+    // Inserting an item in vector V.
+    v = insertItem(v, 10, 7, 137);
+    printf("\nVector V (with a new item):\n");
+    mostrar(v, 11);
+    // It works OK.
+
+    // Removing an item from vector V.
+    v = removeItem(v, 11, 7);
+    printf("\nVector V (without an old item):\n");
+    mostrar(v, 10);
+    // It works OK.
+
+    free(v); // Por cada new tiene que haber un delete.
+    v = NULL; // Me aseguro que el puntero no quede apuntando a memoria que ya no pertenece a mi programa.
+    
+    free(u); // Por cada new tiene que haber un delete.
+    u = NULL; // Me aseguro que el puntero no quede apuntando a memoria que ya no pertenece a mi programa.
+
+    free(cad); // Por cada new tiene que haber un delete.
+    cad = NULL; // Me aseguro que el puntero no quede apuntando a memoria que ya no pertenece a mi programa.
+
+    return 0;
+}
+
+void llenar_int_vector(int *vec, uint8_t cant)
+{
+    srand(time(NULL));
+    for(int i = 0 ; i < cant ; i++)
+    {
+        vec[i] = rand()%100;
+    }
+}
