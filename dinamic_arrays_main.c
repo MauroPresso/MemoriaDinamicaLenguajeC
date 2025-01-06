5b7bc7d9944c101a8edc0aaf804cf4af1d17f614
MauroPresso <mauro.presso@gmail.com>
2025-01-06 19:35:06 -0300

    Ahí les puse nombres distintos a los vectores

--- dinamic_arrays_main.c
+++ dinamic_arrays_main.c
@@ -17,204 +17,204 @@ int main()
     // VECTORES ENTEROS
     printf("\n\n\nVECTORES ENTEROS:");
 
-    int* v = (int*)malloc(10 * sizeof(int));
-    int* u = (int*)malloc(5 * sizeof(int));
+    int* vi = (int*)malloc(10 * sizeof(int));
+    int* ui = (int*)malloc(5 * sizeof(int));
 
     // vector dinamico V
-    llenar_int_vector(v, 10);
+    llenar_int_vector(vi, 10);
     printf("\nVector V:\n");
-    show_int_vector(v, 10);
+    show_int_vector(vi, 10);
 
     // vector dinamico U
-    llenar_int_vector(u, 5);
+    llenar_int_vector(ui, 5);
     printf("\nVector U:\n");
-    show_int_vector(u, 5);
+    show_int_vector(ui, 5);
 
     // Concatenar
-    int *cad = concat_int_vector(v, 10, u, 5);
+    int *cadi = concat_int_vector(vi, 10, ui, 5);
     printf("\nVector concatenado:\n");
-    show_int_vector(cad, 15);
+    show_int_vector(cadi, 15);
 
     // Resizing vector V.
-    v = resize_int_vector(v, 10, 5);
+    vi = resize_int_vector(vi, 10, 5);
     printf("\nVector V resized (new_size < old_size):\n");
-    show_int_vector(v, 5);
+    show_int_vector(vi, 5);
 
-    v = resize_int_vector(v, 5, 10);
+    vi = resize_int_vector(vi, 5, 10);
     printf("\nVector V resized (new_size > old_size):\n");
-    llenar_int_vector(v, 10);
-    show_int_vector(v, 10);
+    llenar_int_vector(vi, 10);
+    show_int_vector(vi, 10);
 
     // Inserting an item in vector V.
-    v = insertItem_int_vector(v, 10, 7, 137);
+    vi = insertItem_int_vector(vi, 10, 7, 137);
     printf("\nVector V (with a new item):\n");
-    show_int_vector(v, 11);
+    show_int_vector(vi, 11);
 
     // Removing an item from vector V.
-    v = removeItem_int_vector(v, 11, 7);
+    vi = removeItem_int_vector(vi, 11, 7);
     printf("\nVector V (without an old item):\n");
-    show_int_vector(v, 10);
+    show_int_vector(vi, 10);
 
-    free(v);
-    free(u);
-    free(cad);
+    free(vi);
+    free(ui);
+    free(cadi);
 
-    v = NULL;
-    u = NULL;
-    cad = NULL;
+    vi = NULL;
+    ui = NULL;
+    cadi = NULL;
     
 
     // VECTORES FLOTANTES
     printf("\n\n\nVECTORES FLOTANTES:");
 
-    float* v = (float*)malloc(10 * sizeof(float));
-    float* u = (float*)malloc(5 * sizeof(float));
+    float* vf = (float*)malloc(10 * sizeof(float));
+    float* uf = (float*)malloc(5 * sizeof(float));
 
     // vector dinamico V
-    llenar_float_vector(v, 10);
+    llenar_float_vector(vf, 10);
     printf("\nVector V:\n");
-    show_float_vector(v, 10);
+    show_float_vector(vf, 10);
 
     // vector dinamico U
-    llenar_float_vector(u, 5);
+    llenar_float_vector(uf, 5);
     printf("\nVector U:\n");
-    show_float_vector(u, 5);
+    show_float_vector(uf, 5);
 
     // Concatenar
-    float* cad = concat_float_vector(v, 10, u, 5);
+    float* cadf = concat_float_vector(vf, 10, uf, 5);
     printf("\nVector concatenado:\n");
-    show_float_vector(cad, 15);
+    show_float_vector(cadf, 15);
 
     // Resizing vector V.
-    v = resize_float_vector(v, 10, 5);
+    vf = resize_float_vector(vf, 10, 5);
     printf("\nVector V resized (new_size < old_size):\n");
-    show_float_vector(v, 5);
+    show_float_vector(vf, 5);
 
-    v = resize_float_vector(v, 5, 10);
+    vf = resize_float_vector(vf, 5, 10);
     printf("\nVector V resized (new_size > old_size):\n");
-    llenar_float_vector(v, 10);
-    show_float_vector(v, 10);
+    llenar_float_vector(vf, 10);
+    show_float_vector(vf, 10);
 
     // Inserting an item in vector V.
-    v = insertItem_float_vector(v, 10, 7, 137);
+    vf = insertItem_float_vector(vf, 10, 7, 137.9876);
     printf("\nVector V (with a new item):\n");
-    show_float_vector(v, 11);
+    show_float_vector(vf, 11);
 
     // Removing an item from vector V.
-    v = removeItem_float_vector(v, 11, 7);
+    vf = removeItem_float_vector(vf, 11, 7);
     printf("\nVector V (without an old item):\n");
-    show_float_vector(v, 10);
+    show_float_vector(vf, 10);
 
-    free(v);
-    free(u);
-    free(cad);
+    free(vf);
+    free(uf);
+    free(cadf);
 
-    v = NULL;
-    u = NULL;
-    cad = NULL;
+    vf = NULL;
+    uf = NULL;
+    cadf = NULL;
 
 
     // VECTORES DE TIPO DOUBLE
     printf("\n\n\nVECTORES DE TIPO DOUBLE:");
 
-    double* v = (double*)malloc(10 * sizeof(double));
-    double* u = (double*)malloc(5 * sizeof(double));
+    double* vd = (double*)malloc(10 * sizeof(double));
+    double* ud = (double*)malloc(5 * sizeof(double));
 
     // vector dinamico V
-    llenar_double_vector(v, 10);
+    llenar_double_vector(vd, 10);
     printf("\nVector V:\n");
-    show_double_vector(v, 10);
+    show_double_vector(vd, 10);
 
     // vector dinamico U
-    llenar_double_vector(u, 5);
+    llenar_double_vector(ud, 5);
     printf("\nVector U:\n");
-    show_double_vector(u, 5);
+    show_double_vector(ud, 5);
 
     // Concatenar
-    double* cad = concat_double_vector(v, 10, u, 5);
+    double* cadd = concat_double_vector(vd, 10, ud, 5);
     printf("\nVector concatenado:\n");
-    show_double_vector(cad, 15);
+    show_double_vector(cadd, 15);
 
     // Resizing vector V.
-    v = resize_double_vector(v, 10, 5);
+    vd = resize_double_vector(vd, 10, 5);
     printf("\nVector V resized (new_size < old_size):\n");
-    show_double_vector(v, 5);
+    show_double_vector(vd, 5);
 
-    v = resize_double_vector(v, 5, 10);
+    vd = resize_double_vector(vd, 5, 10);
     printf("\nVector V resized (new_size > old_size):\n");
-    llenar_double_vector(v, 10);
-    show_double_vector(v, 10);
+    llenar_double_vector(vd, 10);
+    show_double_vector(vd, 10);
 
     // Inserting an item in vector V.
-    v = insertItem_double_vector(v, 10, 7, 137);
+    vd = insertItem_double_vector(vd, 10, 7, 137.98765432);
     printf("\nVector V (with a new item):\n");
-    show_double_vector(v, 11);
+    show_double_vector(vd, 11);
 
     // Removing an item from vector V.
-    v = removeItem_double_vector(v, 11, 7);
+    vd = removeItem_double_vector(vd, 11, 7);
     printf("\nVector V (without an old item):\n");
-    show_double_vector(v, 10);
+    show_double_vector(vd, 10);
 
-    free(v);
-    free(u);
-    free(cad);
+    free(vd);
+    free(ud);
+    free(cadd);
 
-    v = NULL;
-    u = NULL;
-    cad = NULL;
+    vd = NULL;
+    ud = NULL;
+    cadd = NULL;
 
 
     // VECTORES COMPLEJOS
     printf("\n\n\nVECTORES COMPLEJOS:");
 
-    struct Complex* v = (struct Complex*)malloc(10 * sizeof(struct Complex));
-    struct Complex* u = (struct Complex*)malloc(5 * sizeof(struct Complex));
+    struct Complex* vc = (struct Complex*)malloc(10 * sizeof(struct Complex));
+    struct Complex* uc = (struct Complex*)malloc(5 * sizeof(struct Complex));
 
     // vector dinamico V
-    llenar_Complex_vector(v, 10);
+    llenar_Complex_vector(vc, 10);
     printf("\nVector V:\n");
-    show_Complex_vector(v, 10);
+    show_Complex_vector(vc, 10);
 
     // vector dinamico U
-    llenar_Complex_vector(u, 5);
+    llenar_Complex_vector(uc, 5);
     printf("\nVector U:\n");
-    show_Complex_vector(u, 5);
+    show_Complex_vector(uc, 5);
 
     // Concatenar
-    struct Complex* cad = concat_Complex_vector(v, 10, u, 5);
+    struct Complex* cadc = concat_Complex_vector(vc, 10, uc, 5);
     printf("\nVector concatenado:\n");
-    show_Complex_vector(cad, 15);
+    show_Complex_vector(cadc, 15);
 
     // Resizing vector V.
-    v = resize_Complex_vector(v, 10, 5);
+    vc = resize_Complex_vector(vc, 10, 5);
     printf("\nVector V resized (new_size < old_size):\n");
-    show_Complex_vector(v, 5);
+    show_Complex_vector(vc, 5);
 
-    v = resize_Complex_vector(v, 5, 10);
+    vc = resize_Complex_vector(vc, 5, 10);
     printf("\nVector V resized (new_size > old_size):\n");
-    llenar_Complex_vector(v, 10);
-    show_Complex_vector(v, 10);
+    llenar_Complex_vector(vc, 10);
+    show_Complex_vector(vc, 10);
 
     // Inserting an item in vector V.
     struct Complex value;
     value.real = rand() / (float) RAND_MAX;
     value.imag = rand() / (float) RAND_MAX; 
-    v = insertItem_Complex_vector(v, 10, 7, value);
+    vc = insertItem_Complex_vector(vc, 10, 7, value);
     printf("\nVector V (with a new item):\n");
-    show_Complex_vector(v, 11);
+    show_Complex_vector(vc, 11);
 
     // Removing an item from vector V.
-    v = removeItem_Complex_vector(v, 11, 7);
+    vc = removeItem_Complex_vector(vc, 11, 7);
     printf("\nVector V (without an old item):\n");
-    show_Complex_vector(v, 10);
+    show_Complex_vector(vc, 10);
 
-    free(v);
-    free(u);
-    free(cad);
+    free(vc);
+    free(uc);
+    free(cadc);
 
-    v = NULL;
-    u = NULL;
-    cad = NULL;
+    vc = NULL;
+    uc = NULL;
+    cadc = NULL;
 
     // FIN DEL PROGRAMA
     return 0;
