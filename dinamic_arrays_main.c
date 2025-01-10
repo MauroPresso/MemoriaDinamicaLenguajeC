7c1108c6b89363922296d32304ddb704c990494d
MauroPresso <mauro.presso@gmail.com>
2025-01-10 17:49:42 -0300

    Cada prueba de funcion funciona bien por separado pero todas juntas el programa se cuelga.

--- dinamic_arrays_main.c
+++ dinamic_arrays_main.c
@@ -4,23 +4,32 @@
 #include <stdint.h>
 #include <time.h>
 
-void llenar_int_vector(int *vec, uint8_t cant);
+/*void llenar_int_vector(int *vec, uint8_t cant);
 
 void llenar_float_vector(float *vec, uint8_t cant);
 
-void llenar_double_vector(double *vec, uint8_t cant);
+void llenar_double_vector(double *vec, uint8_t cant);*/
 
 void llenar_Complex_vector(struct Complex *vec, uint8_t cant);
 
 int main()
 {
     srand(time(NULL));
-    
+    /*
     // VECTORES ENTEROS
     printf("\n\n\nVECTORES ENTEROS:");
 
     int* vi = (int*)malloc(10 * sizeof(int));
+    if (vi == NULL) {
+        fprintf(stderr, "Error al asignar memoria para vi\n");
+        return 1;
+    }
     int* ui = (int*)malloc(5 * sizeof(int));
+    if (ui == NULL) {
+        fprintf(stderr, "Error al asignar memoria para ui\n");
+        free(vi);
+        return 1;
+    }
 
     // vector dinamico V
     llenar_int_vector(vi, 10);
@@ -34,26 +43,56 @@ int main()
 
     // Concatenar
     int *cadi = concat_int_vector(vi, 10, ui, 5);
+    if (cadi == NULL) {
+        fprintf(stderr, "Error al concatenar vectores\n");
+        free(vi);
+        free(ui);
+        return 1;
+    }
     printf("\nVector concatenado:\n");
     show_int_vector(cadi, 15);
 
     // Resizing vector V.
     vi = resize_int_vector(vi, 10, 5);
+    if (vi == NULL) {
+        fprintf(stderr, "Error al redimensionar vi\n");
+        free(ui);
+        free(cadi);
+        return 1;
+    }
     printf("\nVector V resized (new_size < old_size):\n");
     show_int_vector(vi, 5);
 
     vi = resize_int_vector(vi, 5, 10);
+    if (vi == NULL) {
+        fprintf(stderr, "Error al redimensionar vi\n");
+        free(ui);
+        free(cadi);
+        return 1;
+    }
     printf("\nVector V resized (new_size > old_size):\n");
     llenar_int_vector(vi, 10);
     show_int_vector(vi, 10);
 
     // Inserting an item in vector V.
     vi = insertItem_int_vector(vi, 10, 7, 137);
+    if (vi == NULL) {
+        fprintf(stderr, "Error al insertar elemento en vi\n");
+        free(ui);
+        free(cadi);
+        return 1;
+    }
     printf("\nVector V (with a new item):\n");
     show_int_vector(vi, 11);
 
     // Removing an item from vector V.
     vi = removeItem_int_vector(vi, 11, 7);
+    if (vi == NULL) {
+        fprintf(stderr, "Error al eliminar elemento de vi\n");
+        free(ui);
+        free(cadi);
+        return 1;
+    }
     printf("\nVector V (without an old item):\n");
     show_int_vector(vi, 10);
 
@@ -164,65 +203,83 @@ int main()
     vd = NULL;
     ud = NULL;
     cadd = NULL;
-
-
+    
+    */
     // VECTORES COMPLEJOS
     printf("\n\n\nVECTORES COMPLEJOS:");
 
+    
     struct Complex* vc = (struct Complex*)malloc(10 * sizeof(struct Complex));
     struct Complex* uc = (struct Complex*)malloc(5 * sizeof(struct Complex));
-
     // vector dinamico V
     llenar_Complex_vector(vc, 10);
     printf("\nVector V:\n");
     show_Complex_vector(vc, 10);
-
     // vector dinamico U
     llenar_Complex_vector(uc, 5);
     printf("\nVector U:\n");
     show_Complex_vector(uc, 5);
-
     // Concatenar
-    struct Complex* cadc = concat_Complex_vector(vc, 10, uc, 5);
+    struct Complex* cadc;
+    cadc = concat_Complex_vector(vc, 10, uc, 5);
     printf("\nVector concatenado:\n");
     show_Complex_vector(cadc, 15);
-
-    // Resizing vector V.
-    vc = resize_Complex_vector(vc, 10, 5);
-    printf("\nVector V resized (new_size < old_size):\n");
-    show_Complex_vector(vc, 5);
-
-    vc = resize_Complex_vector(vc, 5, 10);
-    printf("\nVector V resized (new_size > old_size):\n");
-    llenar_Complex_vector(vc, 10);
-    show_Complex_vector(vc, 10);
-
-    // Inserting an item in vector V.
-    struct Complex value;
-    value.real = rand() / (float) RAND_MAX;
-    value.imag = rand() / (float) RAND_MAX; 
-    vc = insertItem_Complex_vector(vc, 10, 7, value);
-    printf("\nVector V (with a new item):\n");
-    show_Complex_vector(vc, 11);
-
-    // Removing an item from vector V.
-    vc = removeItem_Complex_vector(vc, 11, 7);
-    printf("\nVector V (without an old item):\n");
-    show_Complex_vector(vc, 10);
 
     free(vc);
     free(uc);
     free(cadc);
-
+    
     vc = NULL;
     uc = NULL;
     cadc = NULL;
+    
+    
+    // Inserting an item in vector V.
+    struct Complex* voc = (struct Complex*)malloc(10 * sizeof(struct Complex));
+    llenar_Complex_vector(voc, 10);
+    printf("\nVector V:\n");
+    show_Complex_vector(voc, 10);
+    struct Complex value;
+    value.real = rand() / (float) RAND_MAX;
+    value.imag = rand() / (float) RAND_MAX; 
+    voc = insertItem_Complex_vector(voc, 10, 7, value);
+    printf("\nVector V (with a new item):\n");
+    show_Complex_vector(voc, 11);
+    // Removing an item from vector V.
+    voc = removeItem_Complex_vector(voc, 11, 7);
+    printf("\nVector V (without an old item):\n");
+    show_Complex_vector(voc, 10);
+    
+    free(voc);
+    voc = NULL;
 
+    
+    // Resizing vector V.
+    struct Complex* vco = (struct Complex*)malloc(10 * sizeof(struct Complex));
+    llenar_Complex_vector(vco, 10);
+    printf("\nVector V:\n");
+    show_Complex_vector(vco, 10);
+    
+    vco = resize_Complex_vector(vco, 10, 5);
+    printf("\nVector V resized (new_size < old_size):\n");
+    show_Complex_vector(vco, 5);
+
+    vco = resize_Complex_vector(vco, 5, 10);
+    printf("\nVector V resized (new_size > old_size):\n");
+    //llenar_Complex_vector(vco, 10);
+    show_Complex_vector(vco, 10);
+    
+    free(vco);
+    vco = NULL;
+
+    
+    
+    
     // FIN DEL PROGRAMA
     return 0;
 }
 
-void llenar_int_vector(int *vec, uint8_t cant)
+/*void llenar_int_vector(int *vec, uint8_t cant)
 {
     for (int i = 0; i < cant; i++)
     {
@@ -244,13 +301,13 @@ void llenar_double_vector(double *vec, uint8_t cant)
     {
         vec[i] = (rand() / (float) RAND_MAX)*(1.0);
     }
-}
+}*/
 
 void llenar_Complex_vector(struct Complex *vec, uint8_t cant)
 {
     for (int i = 0; i < cant; i++)
     {
-        vec[i].real = (rand() / (float) RAND_MAX)*(1.0);
-        vec[i].imag = (rand() / (float) RAND_MAX)*(1.0);
+        vec[i].real = (rand() / (float) RAND_MAX);
+        vec[i].imag = (rand() / (float) RAND_MAX);
     }
 }
